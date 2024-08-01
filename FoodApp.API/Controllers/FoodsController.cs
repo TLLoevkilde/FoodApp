a55f456fdc953d6fd42e6ec349a1ec67dbc20590
Thomas <tlloevkilde@gmail.com>
2024-08-01 12:39:46 +0200

    Add project files.

---
+++ FoodApp.API/Controllers/FoodsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] {"Kylling", "Bacon"});
+        }
+    }
+}
