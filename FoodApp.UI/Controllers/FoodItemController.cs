625ecc96c0fcd2867d9b3d1568c0d9a684acc3af
Thomas <tlloevkilde@gmail.com>
2024-08-14 10:56:53 +0200

    Table shows in frontend

---
+++ FoodApp.UI/Controllers/FoodItemController.cs
@@ -0,0 +1,38 @@
+﻿using FoodApp.UI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodApp.UI.Controllers
+{
+    public class FoodItemController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public FoodItemController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<FoodItemDto> FoodItems =new List<FoodItemDto>();
+
+            try
+            {
+                var client = httpClientFactory.CreateClient(); // Creates a new client
+
+                var httpResponseMessage = await client.GetAsync("https://localhost:7192/api/fooditem"); // Gets data using backend url
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                FoodItems.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<FoodItemDto>>()); // FoodItems is added to the list
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+
+            return View(FoodItems);  // FoodItem list is added to the View
+        }
+    }
+}
---
+++ FoodApp.UI/Models/DTO/FoodItemDto.cs
@@ -0,0 +1,17 @@
+﻿namespace FoodApp.UI.Models.DTO
+{
+    public class FoodItemDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double ProteinPerHundredGrams { get; set; }
+        public double CalPerHundredGrams { get; set; }
+        public double WeightInGrams { get; set; }
+        public double Price { get; set; }
+        public double ProteinPerWeightInGrams { get; set; }
+        public double CalPerHundredGramsOfProtein { get; set; }
+        public double PricePerHundredGramsOfProtein { get; set; }
+        public double Score { get; set; }
+
+    }
+}
